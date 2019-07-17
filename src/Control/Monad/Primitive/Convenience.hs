8d394961bf6ad8e8117f65598085a606b6bb3b78
chessai <chessai1996@gmail.com>
2019-07-17 11:01:08 -0400

    remove PrimConv

--- src/Control/Monad/Primitive/Convenience.hs
+++ src/Control/Monad/Primitive/Convenience.hs
@@ -1,6 +1,5 @@
 {-# language
-        ConstraintKinds
-      , CPP
+        CPP
       , FlexibleInstances
       , FunctionalDependencies
       , MultiParamTypeClasses
@@ -12,7 +11,6 @@
 |-}
 module Control.Monad.Primitive.Convenience
   ( MonadPrim
-  , PrimConv
   ) where
 
 import Control.Monad.Primitive
@@ -37,30 +35,26 @@ import qualified Control.Monad.Trans.RWS.CPS as CPS
 import GHC.Exts (RealWorld)
 import Control.Monad.ST (ST)
 
--- | A constraint that acts as a witness to some state token 's'
---   is equivalent to the RHS of @'PrimState' m@.
-type PrimConv s m = (PrimMonad m, s ~ PrimState m)
-
 -- | 'PrimMonad''s state token type can be annoying to handle
 --   in constraints. This typeclass lets users notice 'PrimState'
---   less, by implicating @s ~ 'PrimState' m@.
-class PrimConv s m => MonadPrim s m | m -> s where
+--   less, by witnessing @s ~ 'PrimState' m@.
+class (PrimMonad m, s ~ PrimState m) => MonadPrim s m | m -> s where
 
 instance (s ~ RealWorld) => MonadPrim s IO where
 instance MonadPrim s (ST s) where
-instance PrimConv s m => MonadPrim s (ContT r m) where
-instance PrimConv s m => MonadPrim s (IdentityT m) where
-instance PrimConv s m => MonadPrim s (MaybeT m) where
-instance PrimConv s m => MonadPrim s (ExceptT e m) where
-instance PrimConv s m => MonadPrim s (ReaderT r m) where
-instance PrimConv s m => MonadPrim s (Lazy.StateT state m) where
-instance PrimConv s m => MonadPrim s (Strict.StateT state m) where
-instance (Monoid w, PrimConv s m) => MonadPrim s (Lazy.WriterT w m) where
-instance (Monoid w, PrimConv s m) => MonadPrim s (Strict.WriterT w m) where
-instance (Monoid w, PrimConv s m) => MonadPrim s (Lazy.RWST r w state m) where
-instance (Monoid w, PrimConv s m) => MonadPrim s (Strict.RWST r w state m) where
+instance (PrimMonad m, s ~ PrimState m) => MonadPrim s (ContT r m) where
+instance (PrimMonad m, s ~ PrimState m) => MonadPrim s (IdentityT m) where
+instance (PrimMonad m, s ~ PrimState m) => MonadPrim s (MaybeT m) where
+instance (PrimMonad m, s ~ PrimState m) => MonadPrim s (ExceptT e m) where
+instance (PrimMonad m, s ~ PrimState m) => MonadPrim s (ReaderT r m) where
+instance (PrimMonad m, s ~ PrimState m) => MonadPrim s (Lazy.StateT state m) where
+instance (PrimMonad m, s ~ PrimState m) => MonadPrim s (Strict.StateT state m) where
+instance (Monoid w, PrimMonad m, s ~ PrimState m) => MonadPrim s (Lazy.WriterT w m) where
+instance (Monoid w, PrimMonad m, s ~ PrimState m) => MonadPrim s (Strict.WriterT w m) where
+instance (Monoid w, PrimMonad m, s ~ PrimState m) => MonadPrim s (Lazy.RWST r w state m) where
+instance (Monoid w, PrimMonad m, s ~ PrimState m) => MonadPrim s (Strict.RWST r w state m) where
 
 #if MIN_VERSION_transformers(0,5,6)
-instance (Monoid w, PrimConv s m) => MonadPrim s (CPS.WriterT w m) where
-instance (Monoid w, PrimConv s m) => MonadPrim s (CPS.RWST r w state m) where
+instance (Monoid w, PrimMonad m, s ~ PrimState m) => MonadPrim s (CPS.WriterT w m) where
+instance (Monoid w, PrimMonad m, s ~ PrimState m) => MonadPrim s (CPS.RWST r w state m) where
 #endif
