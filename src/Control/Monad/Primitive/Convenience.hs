62331c9b645e5e526f4f28d204f82432df82a7d2
chessai <chessai1996@gmail.com>
2019-09-26 13:20:25 -0400

    improve docs

--- src/Control/Monad/Primitive/Convenience.hs
+++ src/Control/Monad/Primitive/Convenience.hs
@@ -1,10 +1,10 @@
 {-# language
-        FlexibleInstances
-      , FunctionalDependencies
-      , MultiParamTypeClasses
-      , NoImplicitPrelude
-      , TypeFamilies
-      , UndecidableInstances
+    FlexibleInstances
+  , FunctionalDependencies
+  , MultiParamTypeClasses
+  , NoImplicitPrelude
+  , TypeFamilies
+  , UndecidableInstances
   #-}
 
 {-| Convenience typeclass for working with polymorphic @'PrimMonad'@s.
@@ -17,13 +17,15 @@ module Control.Monad.Primitive.Convenience
 import Control.Monad.Primitive
 
 -- | 'PrimMonad''s state token type can be annoying to handle
---   in constraints. This typeclass lets users notice 'PrimState'
---   less, by witnessing @s ~ 'PrimState' m@.
+--   in constraints. This typeclass lets users (visually) notice
+--   'PrimState' equality constraints less, by witnessing that
+--   @s ~ 'PrimState' m@.
 class (PrimMonad m, s ~ PrimState m) => MonadPrim s m | m -> s where
 instance (PrimMonad m, s ~ PrimState m) => MonadPrim s m
 
 -- | 'PrimBase''s state token type can be annoying to handle
---   in constraints. This typeclass lets users notice 'PrimState'
---   less, by witnessing @s ~ 'PrimState' m@.
+--   in constraints. This typeclass lets users (visually) notice
+--   'PrimState' equality constraints less, by witnessing that
+--   @s ~ 'PrimState' m@.
 class (PrimBase m, s ~ PrimState m) => MonadPrimBase s m | m -> s where
 instance (PrimBase m, s ~ PrimState m) => MonadPrimBase s m
